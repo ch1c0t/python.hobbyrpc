c18042b60edbc0767c474377d876ed7ddc56b9da
Anatoly Chernov <chertoly@gmail.com>
2026-05-08 22:01:14 +0300

    A note on how to do 'mkdir -p' with pathlib's Path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,14 @@
 import pytest
+from pathlib import Path
+
 
 @pytest.fixture(scope="session", autouse=True)
 def run_before_suite():
     # Code here runs BEFORE the entire test suite starts
     print("\n[Setup] Initializing global resources...")
+
+    path = Path("~/sources/coffee").expanduser()
+    path.mkdir(parents=True, exist_ok=True)
     
     yield
     
