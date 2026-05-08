dca53edd785cb490bc371549ff013d3dabb92eb4
Anatoly Chernov <chertoly@gmail.com>
2026-05-08 17:55:44 +0300

    Add conftest.py
    uv run pytest -s
    
    https://docs.pytest.org/en/stable/how-to/output.html
    https://docs.pytest.org/en/stable/example/special.html

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+@pytest.fixture(scope="session", autouse=True)
+def run_before_suite():
+    # Code here runs BEFORE the entire test suite starts
+    print("\n[Setup] Initializing global resources...")
+    
+    yield
+    
+    # Code here runs AFTER the entire test suite finishes
+    print("\n[Teardown] Cleaning up global resources...")
