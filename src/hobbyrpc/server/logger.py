75c1212d3499d39762cc23135c13d6f0feeb6b91
Anatoly Chernov <chertoly@gmail.com>
2026-05-16 20:57:22 +0300

    Extract Server logger

--- src/hobbyrpc/server/logger.py
+++ src/hobbyrpc/server/logger.py
@@ -1,10 +1,3 @@
-import sys
 import logging
-
-logging.basicConfig(
-    stream=sys.stderr,
-    level=logging.ERROR,
-    format='%(asctime)s - %(levelname)s - %(message)s'
-)
 
 logger = logging.getLogger(__name__)
