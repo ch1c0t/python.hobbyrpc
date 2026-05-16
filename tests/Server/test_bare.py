551e32ce17a1fa2a375599637ddf98a29a9b00e3
Anatoly Chernov <chertoly@gmail.com>
2026-05-16 23:58:13 +0300

    Test that calling a nonexistent name raises a BadRequest

--- tests/Server/test_bare.py
+++ tests/Server/test_bare.py
@@ -1,4 +1,6 @@
+import pytest
 from hobbyrpc import Client
+from hobbyrpc.client.exceptions import BadRequest
 
 def test_receiving_a_list(bare_rpc):
     call = Client(
@@ -8,3 +10,9 @@ def test_receiving_a_list(bare_rpc):
     list = call('ListNames')
 
     assert list == ['default']
+
+def test_calling_nonexistent_name(bare_rpc):
+    call = Client(bare_rpc.address)
+
+    with pytest.raises(BadRequest):
+        call('NonexistentName')
